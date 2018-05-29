5c1eba022b9a7693fe193803374af4cbc0fc03e7
uridr <orioldomingoroig@gmail.com>
2018-05-29 13:30:35 +0200

    Improved Crawler

--- moogle.py
+++ moogle.py
@@ -116,16 +116,17 @@ def getLinks(soup):
 
 def recursive_crawler(url, expdist, db):
     pages = db["pages"]
-    if expdist > 0:
+    if expdist >= 0:
         if url not in pages:
             soup = getSoup(url)
             pages[url] = soup
             scrapeSite(soup, url, db)
-        links = getLinks(pages[url])
-        for link in links:
-            print(link)
-            link = sanitizeUrl(url,link)
-            recursive_crawler(link,expdist - 1, db)
+        if expdist > 0:
+            links = getLinks(pages[url])
+            for link in links:
+                print(link)
+                link = sanitizeUrl(url,link)
+                recursive_crawler(link,expdist - 1, db)
 
 def crawler(url, maxdist):
     """
